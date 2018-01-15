a08ca52ab8f8c03ea499c47ec87e34faaec85ba4
Ionut Hulub <ionut.hulub@gmail.com>
2018-01-16 01:15:52 +0200

    added search form

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 
 
+def get_hypernym(word, lang='en'):
+    word = word.strip().lower()
+
+    if lang == 'en':
+        if word == 'car':
+            return 'vehicle'
+        if word == 'lion':
+            return 'animal'
+        if word == 'house':
+            return 'building'
+    return None
+
+
 def homepage(request):
-    context = {}
+    context = {
+        'lang': 'en'
+    }
+
+    if request.method == 'POST':
+        search_keyword = request.POST['search_keywords']
+        lang = request.POST['lang']
+        hypernym = get_hypernym(search_keyword, lang=lang)
+        context['hyponym'] = search_keyword
+        context['hypernym'] = hypernym
+        context['lang'] = lang
+
     return render(request, 'homepage/homepage.html', context)
